8c8c450e446070abebe5ab498e9d46c023516b72
trioadhi <trioadhipamungkas04@gmail.com>
2024-09-29 10:08:42 +0700

    Start Assignment 1

---
+++ main.js
@@ -0,0 +1,17 @@
+function process_argv() {
+    const { argv } = process;
+    const result = krsApplication(argv[2], argv[3], argv[4]);
+
+    return result;
+}
+
+function krsApplication(name, programId, gpa) {
+    return '' // TODO: replace this
+}
+
+// Dilarang menghapus/mangganti code dibawah ini!!!
+if (process.env.NODE_ENV !== "test") {
+    console.log(process_argv());
+}
+
+module.exports = krsApplication;
